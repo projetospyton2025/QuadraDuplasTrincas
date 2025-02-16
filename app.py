50c28130c1123681230dd873d6de2c3dafd99a63
projetospyton2025 <marciofernando.maia@yahoo.com.br>
2025-02-16 12:54:31 -0300

    Alterado a porta para o render

--- app.py
+++ app.py
@@ -66,4 +66,5 @@ def download(formato):
         return jsonify({'success': False, 'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
